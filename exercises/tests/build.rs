e8fb7d35eaab1bc8122207a43fc98181e14fa223
Antarecke <2395326872@qq.com>
2024-10-18 09:29:51 +0000

    solved

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -2,21 +2,14 @@
 //!
 //! You should modify this file to make both exercises pass.
 
-use std::env;
-use std::time::SystemTime;
-
 fn main() {
-    // 获取当前时间戳
-    let timestamp = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
+    // In tests7, set up an environment variable called `TEST_FOO`.
+    let timestamp = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs();
-
-    // 设置环境变量 TEST_FOO
-    println!("cargo:rerun-if-changed=build.rs"); // 监测 build.rs 文件变化
     println!("cargo:rustc-env=TEST_FOO={}", timestamp);
 
-    // 启用 "pass" 特性
+    // In tests8, enable "pass" feature to make the testcase return early.
     println!("cargo:rustc-cfg=feature=\"pass\"");
 }
-
